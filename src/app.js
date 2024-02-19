30895b71eca26e7ad03d421fdfb82a85d2cf867e
Gabriel Lococo <127756253+GabrielLococo@users.noreply.github.com>
2024-02-19 19:28:19 -0300

    checkpoint

--- src/app.js
+++ src/app.js
@@ -1,66 +1,56 @@
-require('./database.js')
-const express = require('express')
-const http = require('http')
-const socketIo = require('socket.io')
-const expressHandlebars = require('express-handlebars')
-const ProductManager = require('./dao/db/productManager-db.js')
-const CartManager = require('./dao/db/cartManager-db.js')
-const MessageModel = require('./dao/models/message.model.js') //
-const app = express()
-const server = http.createServer(app)
-const io = socketIo(server)
-const PORT = 8080
-const productManager = new ProductManager()
-const cartManager = new CartManager()
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const PORT = 8080;
+require("./database.js");
 
-// Middleware
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
 
-// Handlebars
-const hbs = expressHandlebars.create({
-    runtimeOptions: {
-        allowProtoPropertiesByDefault: true,
-        allowProtoMethodsByDefault: true,
-    },
-})
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./src/public"));
 
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
-app.set("views", "./src/views")
+//Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
-app.use("/", require("./routes/views.router")(productManager, cartManager))
-app.use('/api/products', require('./routes/products.router')(productManager))
-app.use('/api/carts', require('./routes/cart.router.js')(cartManager, productManager))
-app.use(express.static('./src/public'))
+//Rutas: 
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
 
 
 io.on("connection",  async (socket) => {
-    console.log("Nuevo usuario conectado");
+    console.log("New user ON");
 
-    socket.emit("productos", await productManager.getProducts());    
+    // socket.emit("productos", await productManager.getProducts());    
 
     //agregar producto
 
-    socket.on("addProduct", async (newproducto) => {
-        await productManager.addProduct(newproducto);
+    // socket.on("addProduct", async (newproducto) => {
+    //     await productManager.addProduct(newproducto);
         
-        io.sockets.emit("productos", await productManager.getProducts());
-    });
+    //     io.sockets.emit("productos", await productManager.getProducts());
+    // });
 
     //actualizar producto
 
-    socket.on("updateProduct", async ({ id, updatedProduct }) => {
-        await productManager.updateProduct(id, updatedProduct)
-        io.sockets.emit("products", await productManager.getProducts())
-    })
+    // socket.on("updateProduct", async ({ id, updatedProduct }) => {
+    //     await productManager.updateProduct(id, updatedProduct)
+    //     io.sockets.emit("products", await productManager.getProducts())
+    // })
     
     //eliminar producto
-    socket.on("deleteProduct", async (id) => {
-        await productManager.deleteProduct(id);
+
+    // socket.on("deleteProduct", async (id) => {
+    //     await productManager.deleteProduct(id);
        
-        io.sockets.emit("productos", await productManager.getProducts());
-    });
+    //     io.sockets.emit("productos", await productManager.getProducts());
+    // });
 
 
     // Chat
@@ -77,5 +67,5 @@ io.on("connection",  async (socket) => {
 })
 
 server.listen(PORT, () => {
-    console.log(`Escuchando en el servidor http://localhost:${PORT}`)
+    console.log(`Listening on http://localhost:${PORT}`)
 })
